73c1a149e1f62c5f279901465f98a863e33b223b
Kamei Hidetoshi <kamei.hidetoshi@classmethod.jp>
2022-10-27 08:59:12 +0900

    new project

---
+++ app/src/main/java/com/example/myapplication/MyApp.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import com.example.myapplication.R
+
+class MyApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel(this)
+    }
+
+}
+
+val CHANNEL_ID = "test-channel-id"
+
+private fun createNotificationChannel(context: Context) {
+    // Create the NotificationChannel, but only on API 26+ because
+    // the NotificationChannel class is new and not in the support library
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        val name = context.getString(R.string.channel_name)
+        val descriptionText = context.getString(R.string.channel_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
+        }
+        // Register the channel with the system
+        val notificationManager: NotificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
